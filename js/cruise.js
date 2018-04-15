bbdbe2bc50932644d51eb6b1e5d341890ecdb8c7
MatthewWu5 <china.matthew1157@gmail.com>
2018-04-15 17:35:05 +0800

    refactor class ${index} and support deny

--- js/cruise.js
+++ js/cruise.js
@@ -55,18 +55,29 @@ function GenerateAgentView(agents, element) {
     });
 
     element.appendChild(container);
+
+    var areas = Array.prototype.slice.call(container.querySelectorAll('.agent-info-container .deny-area'));
+    areas.forEach(function (area, index) {
+        area.addEventListener('click', OnAreaClick, true);
+    });
+
+    function OnAreaClick(event) {
+        if (event.currentTarget.parentElement.className.indexOf('denied') != -1) {
+            event.stopPropagation();
+            event.preventDefault();
+        }
+    }
 }
 
-function OnSpecifyResource() {
+function OnSpecifyResource(agentIndex) {
     event.preventDefault();
-    var agentIndexClass = this.getCurrentAgentIndexClass();
-    var popup = document.querySelector('.popup' + agentIndexClass);
+    var popup = document.querySelector('.' + agentIndex + '.popup');
     popup.className = popup.className.replace(/hidden/g, '').trim(' ');
 }
 
-function OnAdd() {
-    var agentIndexClass = this.getCurrentAgentIndexClass();
-    var input = document.querySelector(agentIndexClass + ' .resource-input');
+function OnAdd(agentIndex) {
+    var agentIndexClass = '.' + agentIndex;
+    var input = document.querySelector('.popup' + agentIndexClass + ' .resource-input');
     //Validate
     var labelText = document.querySelector('.popup' + agentIndexClass + ' .label-text');
     var errorText = document.querySelector('.popup' + agentIndexClass + ' .error-text');
@@ -83,34 +94,35 @@ function OnAdd() {
     var self = this;
     input.value.split(', ').forEach(function (value) {
         var div = document.createElement('div');
-        div.className = 'resource ' + agentIndexClass.replace(/\./, '');
+        div.className = 'resource';
         div.addEventListener('click', function () {
-            if (/remove/g.test(event.target.className)) {
-                var agentIndexClass = self.getCurrentAgentIndexClass();
-                document.querySelector('.resource-container' + agentIndexClass).removeChild(event.currentTarget);
-            }
+            self.OnRemove(agentIndex)
         });
         div.innerHTML = '<span class="resource-text">' + value + '</span><i class="remove">+</i>';
-        document.querySelector('.resource-container' + agentIndexClass).appendChild(div);
+        document.querySelector(agentIndexClass + ' .resource-container').appendChild(div);
     });
     input.value = '';
-    this.OnClose();
+    this.OnClose(agentIndex);
 }
 
-function OnClose() {
-    var agentIndexClass = this.getCurrentAgentIndexClass();
-    document.querySelector('.popup' + agentIndexClass).className += ' hidden';
+function OnClose(agentIndex) {
+    document.querySelector('.' + agentIndex + '.popup').className += ' hidden';
 }
 
-function OnRemove() {
+function OnRemove(agentIndex) {
     if (/remove/g.test(event.target.className)) {
-        var agentIndexClass = this.getCurrentAgentIndexClass();
-        document.querySelector('.resource-container' + agentIndexClass).removeChild(event.currentTarget);
+        document.querySelector('.' + agentIndex + ' .resource-container').removeChild(event.currentTarget);
     }
 }
 
-function OnDenyClick() {
+function OnDenyClick(agentIndex) {
     event.preventDefault();
+    var agentContainer = document.querySelector('.agent-info-container.' + agentIndex);
+    if (/^denied|\sdenied\s|\sdenied$/g.test(agentContainer.className)) {
+        agentContainer.className = agentContainer.className.replace(/^denied|\sdenied\s|\sdenied$/g, ' ');
+    } else {
+        agentContainer.className += ' denied';
+    }
 }
 
 function getCurrentAgentIndexClass() {
@@ -121,35 +133,37 @@ function getCurrentAgentIndexClass() {
     return '.' + indexArray[0];
 }
 
-var ResourceTemplate = '<div class="resource ${index}" onclick="OnRemove()"><span class="resource-text">${template}</span><i class="remove">+</i></div>';
+var ResourceTemplate = '<div class="resource" onclick="OnRemove(\'${index}\')"><span class="resource-text">${template}</span><i class="remove">+</i></div>';
 
 //execDenyClass()
 //execResource()
 //execStateClass()
 //execStateValue()
-var AgentTemplate = '<div class="agent-info-container ${execStateClass()}"> \
-                        <div class="circle-icon"></div> \
-                        <div class="content-panel"> \
-                            <div class="row"> \
-                                <div class="agent-name col-md-6" title="${Name}">${Name}</div> \
-                                <div class="rightTop-panel col-md-6" title="| ${execStateValue()} | ${IP} | ${Url}"> \
-                                    | <span>${execStateValue()}</span> | <span>${IP}</span> | <span>${Url}</span> \
+var AgentTemplate = '<div class="agent-info-container ${index} ${execStateClass()}"> \
+                        <div class="deny-area col-md-11"> \
+                            <div class="circle-icon"></div> \
+                            <div class="content-panel"> \
+                                <div class="row"> \
+                                    <div class="agent-name col-md-6" title="${Name}">${Name}</div> \
+                                    <div class="rightTop-panel col-md-6" title="| ${execStateValue()} | ${IP} | ${Url}"> \
+                                        | <span>${execStateValue()}</span> | <span>${IP}</span> | <span>${Url}</span> \
+                                    </div> \
                                 </div> \
-                            </div> \
-                            <div class="row"> \
-                                <div onclick="OnSpecifyResource()" style="display:inline-block" class="${index}"> \
-                                    <span class="plus">+</span> \
-                                    <a href="" class="add-resource">Specify Resource</a> \
-                                </div> \
-                                <div class="resource-container ${index}"> \
-                                    <span>| Resources:</span> \
-                                    ${execResource()} \
+                                <div class="row"> \
+                                    <div onclick="OnSpecifyResource(\'${index}\')" style="display:inline-block"> \
+                                        <span class="plus">+</span> \
+                                        <a href="" class="add-resource">Specify Resource</a> \
+                                    </div> \
+                                    <div class="resource-container"> \
+                                        <span>| Resources:</span> \
+                                        ${execResource()} \
+                                    </div> \
                                 </div> \
                             </div> \
                         </div> \
                         <div class="deny-container ${execDenyClass()}"> \
                             <i></i> \
-                            <a href="" onclick="OnDenyClick()">Deny</a> \
+                            <a href="" onclick="OnDenyClick(\'${index}\')">Deny</a> \
                         </div> \
                     </div> \
                     <div style="position:relative;"> \
@@ -162,7 +176,7 @@ var AgentTemplate = '<div class="agent-info-container ${execStateClass()}"> \
                             <div class="error-text">error format...add like this format 1, 2</div>\
                             <input class="resource-input" type="text"> \
                             <div class="btn-area"> \
-                                <button onclick="OnAdd()" class="${index}">Add resources</button><button onclick="OnClose()" class="${index}">Close</button> \
+                                <button onclick="OnAdd(\'${index}\')">Add resources</button><button onclick="OnClose(\'${index}\')">Close</button> \
                             </div> \
                         </div> \
                     </div> \
